1b4467436223c320d7d8555114a817e3a9a013ef
Anders Porsbo <apor@tv2.dk>
2024-06-06 14:03:00 +0200

    adding output to get caller identity

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# Info added to the developer, so we can figure out where this is being executed
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
